85ae564658b7c1327904d8ef552022b227bc0da0
U-L-M-S <uinclis@outlook.de>
2023-08-16 21:30:43 +0200

    add warning

--- installer.sh
+++ installer.sh
@@ -23,3 +23,29 @@ LOG="install-$(date +%d-%H%M%S).log"
 
 #clear screen
 clear
+
+# Get the width of the terminal
+TERM_WIDTH=$(tput cols)
+
+# Calculate the padding for the message
+MESSAGE="Welcome to my Arch-Hyprland-V3 Installer"
+PAD_LENGTH=$((($TERM_WIDTH - ${#MESSAGE}) / 2))
+
+# Set the color to green
+GREN='\033[0;32m'
+NC='\033[0m' # No Color
+
+# Display the message with thicker width and green color
+printf "${GREN}+$(printf '%*s' "$((TERM_WIDTH - 1))" '' | tr ' ' -)+${NC}\n"
+printf "${GREN}|%*s${MESSAGE}%*s|${NC}\n" $PAD_LENGTH "" $PAD_LENGTH ""
+printf "${GREN}+$(printf '%*s' "$((TERM_WIDTH - 1))" '' | tr ' ' -)+${NC}\n"
+
+# Print password warning message
+printf "\n${YELLOW} Some commands require you to enter your password in order to execute.\n"
+printf " If you are worried about entering your password, you can cancel the script now with CTRL+C and review the contents of this script.${RESET}\n"
+sleep 2
+printf "\n"
+printf "\n"
+
+#clear screen
+clear
